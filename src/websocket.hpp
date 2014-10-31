61de9f44782f820754fe4e782ca41d40b874168a
Tuczi <tuczi007@gmail.com>
2014-10-31 21:55:49 +0000

    include guard added, little refactor

--- src/websocket.hpp
+++ src/websocket.hpp
@@ -1,27 +1,30 @@
-#include <iostream>
+#ifndef TUCZI_WEBSOCKET_HPP
+#define TUCZI_WEBSOCKET_HPP
 
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <netdb.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <string>
 
 #include <openssl/sha.h>
 #include <openssl/hmac.h>
 #include <openssl/evp.h>
 #include <openssl/bio.h>
 #include <openssl/buffer.h>
-#define LINE_END "\n\r"
+
+#include <netdb.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <iostream>
 #include <string>
 #include <sstream>
 
+#define LINE_END "\n\r"
 
 #define SERVER_PORT 9000
 #define QUEUE_SIZE 5
@@ -34,26 +37,26 @@ namespace tuczi {
 class Websocket {
   private:
     const int descriptor;
-    
-    static const size_t BUF_SIZE = 1024;
 
+    static const size_t BUF_SIZE = 1024;
+    
     std::string parseHeandshake(std::string& input);
     void heandshakeResponce(std::string& keyResponce);
     std::string encodeBase64(unsigned char input[SHA_DIGEST_LENGTH]);
     
     int parseFrame(uint8_t * buffer, size_t bufferSize);
     uint8_t* frameHeader(size_t dataSize, size_t& headerSize);
-	
+
   public:
     Websocket(int descriptor_): descriptor(descriptor_) { 
-	  std::string buffer(BUF_SIZE, 0);
+    std::string buffer(BUF_SIZE, 0);
       ::read(descriptor, (void*) buffer.c_str(), BUF_SIZE);
       std::string key = parseHeandshake(buffer);
       heandshakeResponce(key);
-	}
+    }
 
-    int read(uint8_t* buffer, size_t bufferSize);
-    int write(uint8_t* buffer, size_t bufferSize);
+  int read(uint8_t* buffer, size_t bufferSize);
+  int write(uint8_t* buffer, size_t bufferSize);
 };
 }
-
+#endif
